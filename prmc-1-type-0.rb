997c4138eb1163e7f8c97b9fcae2c9a036808338
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2025-03-29 19:56:54 +0900

    Update prmc-1-type-0.rb

--- prmc-1-type-0.rb
+++ prmc-1-type-0.rb
@@ -35,17 +35,43 @@ class M5Unit8Angle
 
   ANGLE8_I2C_ADDR            = 0x43
   ANGLE8_ANALOG_INPUT_8B_REG = 0x10
+  ANGLE8_DIGITAL_INPUT_REG   = 0x20
+  ANGLE8_RGB_24B_REG         = 0x30
 
   def initialize(i2c)
     @i2c = i2c
   end
 
+  def set_led_color_red(ch, value)
+    @i2c.write(ANGLE8_I2C_ADDR, ANGLE8_RGB_24B_REG + ch * 4 + 0, value)
+  rescue StandardError
+    retry  # workaround for Timeout error in I2C
+  end
+
+  def set_led_color_green(ch, value)
+    @i2c.write(ANGLE8_I2C_ADDR, ANGLE8_RGB_24B_REG + ch * 4 + 1, value)
+  rescue StandardError
+    retry  # workaround for Timeout error in I2C
+  end
+
+  def set_led_color_blue(ch, value)
+    @i2c.write(ANGLE8_I2C_ADDR, ANGLE8_RGB_24B_REG + ch * 4 + 2, value)
+  rescue StandardError
+    retry  # workaround for Timeout error in I2C
+  end
+
   def get_analog_input_8bit(ch)
     @i2c.write(ANGLE8_I2C_ADDR, ANGLE8_ANALOG_INPUT_8B_REG + ch)
     @i2c.read(ANGLE8_I2C_ADDR, 1).bytes[0]
-  rescue IOError
-    # workaround for IOError: Timeout error in I2C
-    retry
+  rescue StandardError
+    retry  # workaround for Timeout error in I2C
+  end
+
+  def get_digital_input
+    @i2c.write(ANGLE8_I2C_ADDR, ANGLE8_DIGITAL_INPUT_REG)
+    @i2c.read(ANGLE8_I2C_ADDR, 1).bytes[0]
+  rescue StandardError
+    retry  # workaround for Timeout error in I2C
   end
 end
 
@@ -57,26 +83,42 @@ uart.write "\x90\x3C\x7F"
 sleep 1
 uart.write "\x80\x3C\x40"
 
-i2c1 = I2C.new(unit: :RP2040_I2C1, frequency: 25 * 1000, sda_pin: 6, scl_pin: 7)
+i2c1 = I2C.new(unit: :RP2040_I2C1, frequency: 10 * 1000, sda_pin: 6, scl_pin: 7)
 
 m5_unit_8angle = M5Unit8Angle.new(i2c1)
 
-current_analog_inputs = [nil, nil, nil, nil, nil, nil, nil, nil]
+current_analog_inputs = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
+current_digital_input = nil
+
+LED_OFF_VALUE = 0
+LED_ON_VALUE = 64
+
+(0..7).each do |ch|
+  m5_unit_8angle.set_led_color_red(ch, LED_ON_VALUE)
+  m5_unit_8angle.set_led_color_green(ch, LED_ON_VALUE)
+  m5_unit_8angle.set_led_color_blue(ch, LED_ON_VALUE)
+end
 
 
 # loop
 
 loop do
-  (0..7).each do |i|
-    analog_input = m5_unit_8angle.get_analog_input_8bit(i)
+  (0..7).each do |ch|
+    analog_input = m5_unit_8angle.get_analog_input_8bit(ch)
 
-    if current_analog_inputs[i].nil?
-      current_analog_inputs[i] = analog_input
-      # p [i, analog_input]
-    elsif (analog_input > current_analog_inputs[i] + 1) ||
-          (analog_input < current_analog_inputs[i] - 1)
-      current_analog_inputs[i] = analog_input
-      p [i, current_analog_inputs[i], analog_input, 127 - (analog_input / 2)]
+    if current_analog_inputs[ch].nil?
+      current_analog_inputs[ch] = analog_input
+    elsif (analog_input > current_analog_inputs[ch] + 1) ||
+          (analog_input < current_analog_inputs[ch] - 1)
+      current_analog_inputs[ch] = analog_input
+      p [ch, current_analog_inputs[ch], analog_input, 127 - (analog_input / 2)]
     end
+  end
+
+  digital_input = m5_unit_8angle.get_digital_input()
+
+  if current_digital_input != digital_input
+    current_digital_input = digital_input
+    p [8, digital_input]
   end
 end
