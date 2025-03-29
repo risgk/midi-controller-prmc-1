b152b0239a5c90cdd9163efaf57bfc62ad015126
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2025-03-30 07:56:42 +0900

    Update prmc-1-type-0.rb

--- prmc-1-type-0.rb
+++ prmc-1-type-0.rb
@@ -48,6 +48,12 @@ class M5UnitAngle8
     retry  # workaround for Timeout error in I2C
   end
 
+  def set_led_color_blue(ch, value)
+    @i2c.write(ANGLE8_I2C_ADDR, ANGLE8_RGB_24B_REG + ch * 4 + 2, value)
+  rescue StandardError
+    retry  # workaround for Timeout error in I2C
+  end
+
   def get_analog_input_8bit(ch)
     @i2c.write(ANGLE8_I2C_ADDR, ANGLE8_ANALOG_INPUT_8B_REG + ch)
     @i2c.read(ANGLE8_I2C_ADDR, 1).bytes[0]
@@ -66,54 +72,65 @@ end
 
 class PRMC1Core
   def initialize
+    @led_byte = 0xFF
   end
 
   def on_parameter_changed(key, value)
+    p [key, value]
+  end
+
+  def get_led_byte
+    @led_byte
   end
 end
 
 
 # setup
 
-ANGLE8_LED_ON_VALUE = 1
+LED_ON_VALUE = 1
 
 uart = UART.new(unit: :RP2040_UART1, txd_pin: 4, rxd_pin: 5, baudrate: 31250)
 uart.write "\x90\x3C\x7F"
 sleep 1
 uart.write "\x80\x3C\x40"
 
-i2c1 = I2C.new(unit: :RP2040_I2C1, frequency: 50 * 1000, sda_pin: 6, scl_pin: 7)
+i2c1 = I2C.new(unit: :RP2040_I2C1, frequency: 25 * 1000, sda_pin: 6, scl_pin: 7)
 angle8 = M5UnitAngle8.new(i2c1)
 prmc_1_core = PRMC1Core.new
 
-current_angle8_analog_input_array = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
-current_angle8_digital_input      = nil
-current_angle8_led_green_byte     = 0xFF
+current_analog_input_array = [nil, nil, nil, nil, nil, nil, nil, nil, nil]
+current_digital_input      = nil
 
 
 # loop
 
 loop do
   (0..7).each do |ch|
-    angle8_analog_input_8bit = angle8.get_analog_input_8bit(ch)
+    analog_input = angle8.get_analog_input_8bit(ch)
 
-    if current_angle8_analog_input_array[ch].nil?
-      current_angle8_analog_input_array[ch] = angle8_analog_input_8bit
-    elsif (angle8_analog_input_8bit > current_angle8_analog_input_array[ch] + 1) ||
-          (angle8_analog_input_8bit < current_angle8_analog_input_array[ch] - 1)
-      current_angle8_analog_input_array[ch] = angle8_analog_input_8bit
-      p [ch, current_angle8_analog_input_array[ch], angle8_analog_input_8bit, 127 - (angle8_analog_input_8bit / 2)]
+    if current_analog_input_array[ch].nil?
+      current_analog_input_array[ch] = analog_input
+    elsif (analog_input > current_analog_input_array[ch] + 1) ||
+          (analog_input < current_analog_input_array[ch] - 1)
+      current_analog_input_array[ch] = analog_input
+      prmc_1_core.on_parameter_changed(ch, 127 - (analog_input / 2))
     end
   end
 
-  angle8_digital_input = angle8.get_digital_input()
+  digital_input = angle8.get_digital_input()
 
-  if current_angle8_digital_input != angle8_digital_input
-    current_angle8_digital_input = angle8_digital_input
-    p [8, angle8_digital_input]
+  if current_digital_input != digital_input
+    current_digital_input = digital_input
+    prmc_1_core.on_parameter_changed(8, digital_input)
   end
 
-  (0..7).each do |ch|
-    angle8.set_led_color_green(ch, ((current_angle8_led_green_byte >> ch) & 0x01) * ANGLE8_LED_ON_VALUE)
+  led_byte_output = prmc_1_core.get_led_byte()
+
+  (0..3).each do |ch|
+    angle8.set_led_color_blue(ch, ((led_byte_output >> ch) & 0x01) * LED_ON_VALUE)
+  end
+
+  (4..7).each do |ch|
+    angle8.set_led_color_green(ch, ((led_byte_output >> ch) & 0x01) * LED_ON_VALUE)
   end
 end
