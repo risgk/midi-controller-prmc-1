4bb119501ea4ba7f23d20c1ad0f719af1329db48
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2025-04-06 09:37:44 +0900

    Update prmc-1-type-0.rb

--- prmc-1-type-0.rb
+++ prmc-1-type-0.rb
@@ -153,8 +153,8 @@ class PRMC1Core
     @root_degrees_candidate = []
     @arpeggio_intervals = []
     @arpeggio_intervals_candidate = []
-    @number_of_sub_step = 8
-    @number_of_sub_step_candidate = 8
+    @number_of_step_division = 8
+    @number_of_step_division_candidate = 8
     @scale_notes = [-1, 48, 50, 52, 53, 55, 57, 59,
                         60, 62, 64, 65, 67, 69, 71,
                         72, 74, 76, 77, 79, 81, 83]
@@ -198,22 +198,22 @@ class PRMC1Core
       case arpeggio_pattern
       when 1
         @arpeggio_intervals_candidate = [1, 3, 5, 7, 1, 3, 5, 7]
-        @number_of_sub_step_candidate = 8
+        @number_of_step_division_candidate = 8
       when 2
         @arpeggio_intervals_candidate = [1, 3, 5, 7, 5, 3, 1, 3]
-        @number_of_sub_step_candidate = 8
+        @number_of_step_division_candidate = 8
       when 3
         @arpeggio_intervals_candidate = [1, 3, 5, 1, 3, 5, 1, 3]
-        @number_of_sub_step_candidate = 8
+        @number_of_step_division_candidate = 8
       when 4
         @arpeggio_intervals_candidate = [1, 3, 5, 3, 1, 3, 5, 3]
-        @number_of_sub_step_candidate = 8
+        @number_of_step_division_candidate = 8
       when 5
         @arpeggio_intervals_candidate = [1, 4, 5, 1, 4, 5, 1, 4]
-        @number_of_sub_step_candidate = 8
+        @number_of_step_division_candidate = 8
       when 6
         @arpeggio_intervals_candidate = [1, 4, 5, 4, 1, 4, 5, 4]
-        @number_of_sub_step_candidate = 8
+        @number_of_step_division_candidate = 8
       end
 
       set_parameter_status_bits(arpeggio_pattern)
@@ -292,19 +292,19 @@ class PRMC1Core
       @step = 0 if @step == 4
       @root_degrees_candidate.each_with_index { |n, idx| @root_degrees[idx] = n }
       @arpeggio_intervals_candidate.each_with_index { |n, idx| @arpeggio_intervals[idx] = n }
-      @number_of_sub_step = @number_of_sub_step_candidate
+      @number_of_step_division = @number_of_step_division_candidate
       set_step_status_bits(@step + 1)
     end
 
-    if @clock_count % (96 / @number_of_sub_step) == (GATE_TIME * 16) / @number_of_sub_step
+    if @clock_count % (96 / @number_of_step_division) == (GATE_TIME * 16) / @number_of_step_division
       if @playing_note != -1
         @midi.send_note_off(@playing_note, NOTE_OFF_VELOCITY, @midi_channel)
       end
     end
 
-    if @clock_count % (96 / @number_of_sub_step) == 0
+    if @clock_count % (96 / @number_of_step_division) == 0
       root = @root_degrees[@step]
-      interval = @arpeggio_intervals[@clock_count / (96 / @number_of_sub_step)]
+      interval = @arpeggio_intervals[@clock_count / (96 / @number_of_step_division)]
 
       if root > 0 && interval > 0
         note_index = root + interval - 1
